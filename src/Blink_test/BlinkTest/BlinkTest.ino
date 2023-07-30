894d3ac35126ef7b19119b94be6ca75d12818f6a
KKallas <kaspar@digitalsputnik.com>
2023-07-31 01:05:35 +0300

    Arduino Guide
    #2

---
+++ src/Blink_test/BlinkTest/BlinkTest.ino
@@ -0,0 +1,23 @@
+const int pwmPin = 4;
+const int pwmFrequency = 19000; // 19 kHz
+const int pwmResolution = 10;   // 10-bit resolution (1024 levels)
+const int pwmValue1 = 0;
+const int pwmValue2 = 256;
+const int blinkFrequency = 400; // 400 Hz (alternating between pwmValue1 and pwmValue2)
+
+void setup() {
+  pinMode(27, OUTPUT); // Set IO27 as output
+  digitalWrite(27, HIGH); // Enable output on IO27 (set it to HIGH)
+  
+  ledcSetup(0, pwmFrequency, pwmResolution); // Configure PWM
+  ledcAttachPin(pwmPin, 0); // Attach PWM to IO4 (Channel 0)
+}
+
+void loop() {
+  // Alternately set the PWM duty cycle to pwmValue1 and pwmValue2
+  ledcWrite(0, pwmValue1);
+  delayMicroseconds(1000000 / (2 * blinkFrequency)); // Half-period delay
+
+  ledcWrite(0, pwmValue2);
+  delayMicroseconds(1000000 / (2 * blinkFrequency)); // Half-period delay
+}
