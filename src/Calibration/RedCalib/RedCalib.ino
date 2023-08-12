5dcc1cab24c883370d81c44470ff32ba94969ea4
KKallas <kaspar@digitalsputnik.com>
2023-08-12 22:27:43 +0300

    intermid
    #3

--- src/Calibration/RedCalib/RedCalib.ino
+++ src/Calibration/RedCalib/RedCalib.ino
@@ -11,11 +11,14 @@ const int blinkFrequency = 200;
 unsigned long lastUpdate = millis();
 unsigned long now = millis();
 
-int pwmValue1 = 0;
-int pwmValue2 = 0;
+int pwmValueRed = 0;
+int pwmValueGreen = 0;
+int pwmValueBlue = 0;
+int pwmValueWhite = 0;
+int pwmValueFan = 0;
 int currentTempData = 0;
 
-char receivedChars[32];             // an array to store the received UART data
+char receivedChars[64];             // an array to store the received UART data
 boolean newData = false;            // if all characters until newline are recieved
 
 void ColorUpdate( void * pvParameters ){
@@ -25,12 +28,21 @@ void ColorUpdate( void * pvParameters ){
 
     ledcSetup(0, pwmFrequency, pwmResolution); // Configure PWM
     ledcSetup(1, pwmFrequency, pwmResolution); // Configure PWM
-    ledcAttachPin(pwmPin, 0);       // Attach PWM to IO22 (Red Channel)
-    ledcAttachPin(0, 1);            // Fan
+    ledcSetup(2, pwmFrequency, pwmResolution); // Configure PWM
+    ledcSetup(3, pwmFrequency, pwmResolution); // Configure PWM
+    ledcSetup(4, pwmFrequency, pwmResolution); // Configure PWM
+    ledcAttachPin(21, 0);           // Attach PWM to IO21 (Red Channel)
+    ledcAttachPin(19, 1);           // Attach PWM to IO19 (Green Channel) 
+    ledcAttachPin(18, 2);           // Attach PWM to IO18 (Blue Channel)
+    ledcAttachPin(4, 3);            // Attach PWM to IO04 (White Channel) 
+    ledcAttachPin(0, 4);            // Fan IO0
 
-    while(true){                    // Alternately set the PWM duty cycle to pwmValue1 and pwmValue2, Half-period delay
-      ledcWrite(0, pwmValue1*8);
-      ledcWrite(1, pwmValue2*8);
+    while(true){
+      ledcWrite(0, pwmValueRed);
+      ledcWrite(1, pwmValueGreen);
+      ledcWrite(2, pwmValueBlue);
+      ledcWrite(3, pwmValueWhite);
+      ledcWrite(4, pwmValueFan);
       delayMicroseconds(1000000 / (blinkFrequency)); 
     }
  
@@ -39,9 +51,6 @@ void ColorUpdate( void * pvParameters ){
 void setup() {
  
   Serial.begin(115200);
- 
-  Serial.print("Creating services ");
-  Serial.println(taskCore);
 
   pinMode(27, OUTPUT);              // Set IO27 as output
   digitalWrite(27, HIGH);           // Enable output on IO27 (set it to HIGH)  
@@ -56,14 +65,12 @@ void setup() {
                     0,              // Priority of the task
                     NULL,           // Task handle.
                     taskCore);      // Core where the task should run
- 
-  Serial.println("Task created...");
-  Serial.println("Starting main loop...");
 }
  
 void loop() {
   int temperatureData;
   int bytesAvailable;
+  now = millis();
 
   if (now - lastUpdate > 1000) {
     Wire.requestFrom(78,2);
@@ -75,6 +82,7 @@ void loop() {
 
     /* Print the received start code - it's usually 0. */
     //Serial.printf("time:%i,dmx_fan:%02X,dmx_val:%02X,temp:%f\n", lastUpdate, data[511], data[512],temperatureData*0.125);
+    //Serial.printf("time:%i\n", lastUpdate);
     lastUpdate = now;
     
   }
@@ -94,8 +102,8 @@ void RecvUart() {
     if (rc != endMarker) {
       receivedChars[ndx] = rc;
       ndx++;
-      if (ndx >= 32) {
-        ndx = 32 - 1;
+      if (ndx >= 64) {
+        ndx = 64 - 1;
       }
     }
     else {
@@ -107,19 +115,35 @@ void RecvUart() {
 }
 
 void HandleUartCmd() {
-    char IntMS;
-    char IntLS;
-    int Intensity;
+    int IntensityRed, IntensityGreen, IntensityBlue, IntensityWhite;
     int Fan;
 
     if (newData == true && receivedChars[0] == 'A') {
-      IntMS = receivedChars[1];
-      IntLS = receivedChars[2];
-      Fan = receivedChars[3];
+      // Red Channel
+      IntensityRed = twoByteChar(receivedChars[1],receivedChars[2]);
+      // Green Channel
+      IntensityGreen = twoByteChar(receivedChars[3],receivedChars[4]);
+      // Blue Channel
+      IntensityBlue = twoByteChar(receivedChars[5],receivedChars[6]);
+      // White Channel
+      IntensityWhite = twoByteChar(receivedChars[7],receivedChars[8]);
+      // Fan
+      Fan = receivedChars[9];
 
-      Intensity = ((int)IntMS << 8) | (int)(IntLS & 0xFF);
+      Serial.printf("\"red_val\":%X,\"green_val\":%X,\"blue_val\":%X,\"white_val\":%X,\"fan_val\":%X,\"temp\":%f\n", IntensityRed, IntensityGreen, IntensityBlue, IntensityWhite, Fan, currentTempData*0.125);
+      //Serial.printf("dmx_fan:%01X,temp:%f\n", Fan, currentTempData*0.125);
 
-      Serial.printf("dmx_val:%02X,dmx_fan:%02X,temp:%f\n", Intensity, Fan, currentTempData*0.125);
+      pwmValueRed = IntensityRed;
+      pwmValueGreen = IntensityGreen;
+      pwmValueBlue = IntensityBlue;
+      pwmValueWhite = IntensityWhite;
+      pwmValueFan = Fan;
+      
       newData = false;
   }
+}
+
+int twoByteChar(char byte1,char byte2) {
+  int output = ((int)byte1 << 8) | (int)(byte2 & 0xFF);
+  return(output);
 }
